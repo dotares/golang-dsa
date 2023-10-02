eacdb188260e8dad88c0b397f70cec82693893b3
fibbbonachos <arish2k3@gmail.com>
2023-10-02 13:29:46 +0530

    print the linked list data

--- main.go
+++ main.go
@@ -21,16 +21,31 @@ func (l *linkedList) prepend(n *node) {
 	l.length++
 }
 
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Println("\n")
+}
+
 func main() {
 	myList := linkedList{}
 
 	node1 := &node{data: 48}
 	node2 := &node{data: 18}
 	node3 := &node{data: 16}
+	node4 := &node{data: 90}
+	node5 := &node{data: 12}
+	node6 := &node{data: 88}
 
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
-
-	fmt.Println(myList)
+	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.prepend(node6)
+	myList.printListData()
 }
